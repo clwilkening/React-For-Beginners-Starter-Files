7383a45cb61a730cefa2e8f1780dc45d4c358f3f
Chris Wilkening <clw13clw13@gmail.com>
2016-12-26 14:31:13 -0500

    video 6 added classes

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Componenet {
+  render() {
+    return(
+      <p>Header</p>
+    )
+  }
+}
+
+export default Header;
---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Order extends React.Componenet {
+  render() {
+    return(
+      <p>Order
+      </p>
+    )
+  }
+}
+
+export default Order;
