aed557db8f782eadf4fd80d4349500592995e200
Chris Wilkening <clw13clw13@gmail.com>
2016-12-30 09:59:26 -0500

    vid 17 synced firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyBUD4EzLhurSBZ8LD75Du-nl8Ky2Bmq3JM",
+  authDomain: "catch-of-the-day-chris-w.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-chris-w.firebaseio.com"
+});
+
+export default base;
